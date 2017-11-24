399b4fba501a5546263ad9773d8f0c3c03abfdf8
Brian Phillips <brian.phillips@SL-ML-0001459.local>
2017-11-23 18:16:04 -0800

    hooking up redux and some "hello world"  components

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Constants } from 'expo';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { purple } from './utils/colors';
+import AddDeck from './components/AddDeck';
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <AddDeck />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/AddDeck.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class AddDeck extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>AddDeck</Text>
+      </View>
+    )
+  }
+}
+
+export default AddDeck;
