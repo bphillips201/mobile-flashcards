e9d7d90801cbef42cb4117bb4956acf3ff8bc223
Brian Phillips <brian.phillips@SL-ML-0001459.local>
2017-11-23 18:36:31 -0800

    setting up basic tab navigation

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { Constants } from 'expo';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import reducer from './reducers'
-import { purple } from './utils/colors';
+import { purple, white } from './utils/colors';
 import AddDeck from './components/AddDeck';
+import DeckList from './components/DeckList';
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
@@ -15,13 +18,45 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
   )
 }
 
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
+        <View style={{flex: 1}}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <AddDeck />
+          <Tabs />
         </View>
       </Provider>
     );
---
+++ components/DeckList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class DeckList extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>DeckList</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckList;
