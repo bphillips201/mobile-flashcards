afd3a1a3894f0abfa5914d28219a20637ebae1fd
Brian Phillips <brian.phillips@SL-ML-0001459.local>
2017-11-24 15:36:44 -0800

    setting up additional api/redux calls

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,6 @@
-export const ADD_DECK_TITLE = 'ADD_DECK';
 export const GET_DECKS = 'GET_DECKS';
+export const ADD_DECK_TITLE = 'ADD_DECK';
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK';
 
 export function getDecks (decks) {
   return {
@@ -13,5 +14,13 @@ export function addDeckTitle (title, id) {
     type: ADD_DECK_TITLE,
     id,
     title
+  }
+}
+
+export function addCardToDeck (title, card) {
+  return {
+    type: ADD_DECK_TITLE,
+    title: title,
+    card
   }
 }
--- utils/api.js
+++ utils/api.js
@@ -9,6 +9,12 @@ export function fetchDecks() {
     .then(data => data);
 }
 
+export function fetchDeck(id) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then(res => JSON.parse(res))
+    .then(data => data.filter((item) => item.id === id));
+}
+
 export function submitDeckTitle (title) {
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
     [title]: {
